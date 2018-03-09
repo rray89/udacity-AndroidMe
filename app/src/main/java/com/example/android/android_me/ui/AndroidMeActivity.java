743e817cdf64a1c150bff5fa5f40c37904a26814
rray89 <rray@hotmail.ca>
2018-03-09 14:24:21 -0400

    finished TF.03

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -32,29 +32,35 @@ public class AndroidMeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_me);
 
-        // COMPLETED TF.01 TODO (1.5) Create a new BodyPartFragment instance and display it using the FragmentManager
-        BodyPartFragment headFragment = new BodyPartFragment();
+        // COMPLETED TF.03 TODO (3.5) Only create new fragments when there is no previously saved state
+        if (savedInstanceState == null) {
 
-        // COMPLETED TF.02 TODO (2.4) Set the list of image id's for the head fragment and set the position to the second image in the list
-        headFragment.setmImageIds(AndroidImageAssets.getHeads());
-        headFragment.setmListIndex(1);
+            // COMPLETED TF.01 TODO (1.5) Create a new BodyPartFragment instance and display it using the FragmentManager
+            BodyPartFragment headFragment = new BodyPartFragment();
 
-        // Add the fragment to its container using a FragmentManager and a Transaction
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .add(R.id.head_container, headFragment)
-                .commit();
+            // COMPLETED TF.02 TODO (2.4) Set the list of image id's for the head fragment and set the position to the second image in the list
+            headFragment.setImageIds(AndroidImageAssets.getHeads());
+            headFragment.setListIndex(1);
 
-        // COMPLETED TF.02 TODO (2.5) Create and display the body and leg BodyPartFragments
-        BodyPartFragment bodyFragment = new BodyPartFragment();
-        fragmentManager.beginTransaction()
-                .add(R.id.body_container, bodyFragment)
-                .commit();
+            // Add the fragment to its container using a FragmentManager and a Transaction
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .add(R.id.head_container, headFragment)
+                    .commit();
 
-        BodyPartFragment legFragment = new BodyPartFragment();
-        fragmentManager.beginTransaction()
-                .add(R.id.leg_container, legFragment)
-                .commit();
+            // COMPLETED TF.02 TODO (2.5) Create and display the body and leg BodyPartFragments
+            BodyPartFragment bodyFragment = new BodyPartFragment();
+            bodyFragment.setImageIds(AndroidImageAssets.getBodies());
+            fragmentManager.beginTransaction()
+                    .add(R.id.body_container, bodyFragment)
+                    .commit();
+
+            BodyPartFragment legFragment = new BodyPartFragment();
+            legFragment.setImageIds(AndroidImageAssets.getLegs());
+            fragmentManager.beginTransaction()
+                    .add(R.id.leg_container, legFragment)
+                    .commit();
+        }
 
     }
 }
--- app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -12,11 +12,16 @@ import android.widget.ImageView;
 import com.example.android.android_me.R;
 import com.example.android.android_me.data.AndroidImageAssets;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // COMPLETED TF.01 TODO (1.2) Create a new class called BodyPartFragment to display an image of an Android-Me body part
 // In this class, you'll need to implement an empty constructor and the onCreateView method
 public class BodyPartFragment extends Fragment {
+
+    // TF.03 TODO (3.3) Create final Strings to store state information about the list of images and list index
+    public static final String IMAGE_ID_LIST = "image_ids";
+    public static final String LIST_INDEX = "list_index";
 
     //Tag for logging
     private static final String TAG = "BodyPartFragment";
@@ -33,6 +38,13 @@ public class BodyPartFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //Load the saved state (the list of images and list index) if there is one
+        if (savedInstanceState != null) {
+            mImageIds = savedInstanceState.getIntegerArrayList(IMAGE_ID_LIST);
+            mListIndex = savedInstanceState.getInt(LIST_INDEX);
+        }
+
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
 
@@ -43,6 +55,25 @@ public class BodyPartFragment extends Fragment {
         if (mImageIds != null) {
             // COMPLETED TF.02 TODO (2.3) If a list of image ids exists, set the image resource to the correct item in that list
             imageView.setImageResource(AndroidImageAssets.getHeads().get(0));
+
+            // COMPLETED TF.03 TODO (3.1) Set a click listener on the image view and on a click increment the list index and set the image resource
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (mListIndex < mImageIds.size() - 1) {
+                        mListIndex++;
+                    }
+                    // COMPLETED TF.03 TODO (3.2) If you reach the end of a list of images, set the list index back to 0 (the first item in the list)
+                    else {
+                        mListIndex = 0;
+                    }
+                }
+            });
+
+
+            imageView.setImageResource(mImageIds.get(mListIndex));
+
+
         } else { // Otherwise, create a Log statement that indicates that the list was not found
             Log.v(TAG, "This fragment has a null list of image ids. ");
         }
@@ -51,15 +82,20 @@ public class BodyPartFragment extends Fragment {
     }
 
     // COMPLETED TF.02 TODO (2.1) Create a setter method and class variable to set and store of a list of image resources
-    public void setmImageIds(List<Integer> imageIds) {
+    public void setImageIds(List<Integer> imageIds) {
         mImageIds = imageIds;
     }
 
     // COMPLETED TF.02 TODO (2.2) Create another setter method and variable to track and set the index of the list item to display
     // ex. index = 0 is the first image id in the given list , index 1 is the second, and so on
-    public void setmListIndex(int listIndex) {
+    public void setListIndex(int listIndex) {
         mListIndex = listIndex;
     }
 
-
+    // COMPLETED TF.03 TODO (3.4) Override onSaveInstanceState and save the current state of this fragment
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle currentState) {
+        currentState.putIntegerArrayList(IMAGE_ID_LIST, (ArrayList<Integer>) mImageIds);
+        currentState.putInt(LIST_INDEX, mListIndex);
+    }
 }
